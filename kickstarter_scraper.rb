ba4cd9a76765ad21c30a80f0eaa613b82affe86c
Alexander Bispo <alexbispo173@gmail.com>
2019-11-19 22:56:01 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -16,10 +16,12 @@ def create_project_hash
   
  projects = {}
  
- kick
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.hbcard_name strong a").text
+      projects[title.to_sym]
   
  
-  
+  projects
 end
 
 create_project_hash
